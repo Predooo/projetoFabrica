d64a150285ae938814caee9cf8de8cc773a25c74
Predooo <pedropotulski19@gmail.com>
2022-12-06 12:26:26 -0300

    banco

--- src/bd/bd.js
+++ src/bd/bd.js
@@ -1,7 +1,7 @@
 //Conexao com o BD
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('sistemaloja', 'root', 'predolinde',{
+const sequelize = new Sequelize('sistemaloja', 'root', 'mynewpassword',{
     host: '127.0.0.1',
     port: 3306,
     dialect: 'mysql'
